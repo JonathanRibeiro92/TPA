db4689b3ace7673e15cd5f3040fe52d770280a00
Jay <jota.ribeiro.silva@gmail.com>
2018-04-11 17:08:36 -0300

    Create Primo.java

---
+++ tadDicionario/src/tadDicionario/Primo.java
@@ -0,0 +1,29 @@
+public class Primo {
+
+    private static boolean isprimo(int n)
+    {
+        int qtdDiv = 0, div =1;
+        int limit = (int)Math.sqrt(n);
+
+        while(div < limit)
+        {
+            if((n%div)==0)
+            {
+                qtdDiv++;
+            }
+
+            div ++;
+        }
+
+        return qtdDiv==1;
+    }
+
+    public static int primoMaiorqN(int n)
+    {
+        if(isprimo(n))
+        {
+            return n;
+        }
+        return primoMaiorqN(n+1);
+    }
+}
